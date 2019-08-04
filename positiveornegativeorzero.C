c98918f6311a51d66dec69457943cb7d53d8b425
Aditya Janjanam <51488353+adityajanjanam@users.noreply.github.com>
2019-08-04 18:56:21 +0530

    Update positiveornegativeorzero.C

--- positiveornegativeorzero.C
+++ positiveornegativeorzero.C
@@ -5,11 +5,11 @@ int main()
 int n;
 scanf("%d",&n);
 if(n>0)
-printf("%d is a positve number",n);
+printf("positve");
 else if(n<0)
-printf("%d is a negative number",n);
+printf("Negative");
 else 
-printf("%d i a zero",n);
+printf("Zero",n);
 
     return 0;
 }
